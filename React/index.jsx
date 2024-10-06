df10537278b8a41a2a07c0a58ed986349af67e69
SubairMohamed <SubairMohamed44@gmail.com>
2024-10-06 23:05:48 +0100

    well

--- React/index.jsx
+++ React/index.jsx
@@ -1,1 +1,11 @@
 const JSX = <h1>Hello JSX!</h1>;
+
+const JSX = <div>
+  <h1>hello react</h1>
+  <p>this is react</p>
+  <ul>
+    <li>tea</li>
+    <li>coffee</li>
+    <li>well</li>
+  </ul>
+</div>;
