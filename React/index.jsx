4911994548d5297e4490013959ddd689e3fd9195
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-10-12 13:08:36 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -37,3 +37,20 @@
 //        <div className="customComponent">hello</div>
 //      );
 //   }
+
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     // Change code below this line
+
+//      return (
+//        <div>
+//        <h1>Hello React!</h1>
+//        </div>
+//      )
+
+//     // Change code above this line
+//   }
+// };
