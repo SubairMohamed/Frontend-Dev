5be3c2e6d4dd0609ec2534dba47be7783f7898dc
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-10-17 12:12:49 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -54,3 +54,30 @@
 //     // Change code above this line
 //   }
 // };
+
+// component composition
+// const ChildComponent = () => {
+//   return (
+//     <div>
+//       <p>I am the child</p>
+//     </div>
+//   );
+// };
+
+// class ParentComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>I am the parent</h1>
+//         { /* Change code below this line */ }
+//         <ChildComponent />
+
+//         { /* Change code above this line */ }
+//       </div>
+//     );
+//   }
+// };
+
