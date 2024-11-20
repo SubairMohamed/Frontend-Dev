529630d9c02aa5fb4988802c49bb745e267677a6
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-20 11:35:08 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -158,4 +158,24 @@
 //     );
 //   }
 // };
+
+//Render a Class Component to the DOM
+// class TypesOfFood extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Types of Food:</h1>
  
+//     <Fruits />
+//     <Vegetables />
+        
+//       </div>
+//     );
+//   }
+// };
+
+// // Change code below this line
+// ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
