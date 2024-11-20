0b0724c68ce2dd0e98980af30836e909cb6de408
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-20 11:56:35 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -179,3 +179,21 @@
 
 // // Change code below this line
 // ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
+
+// Write a React Component from Scratch
+// class MyComponent extends React.Component {
+
+//   constructor (props) {
+//     super (props);
+//   }
+
+//   render () {
+//     return (
+//     <div>
+//       <h1>My First React Component!</h1>
+//     </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
