029307dba2d8b5a647589790eb830dadfc2da20e
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-22 12:06:05 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -224,3 +224,29 @@ const CurrentDate = (props) => {
 //     );
 //   }
 // };
+
+// Pass an Array props
+// const List = (props) => {
+//   { /* Change code below this line */ }
+//   return <p>{props.tasks.join(", ")}</p>;
+//   { /* Change code above this line */ }
+// };
+
+// class ToDo extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>To Do Lists</h1>
+//         <h2>Today</h2>
+//         { /* Change code below this line */ }
+//          <List tasks={["Walk", "Cook", "Bake"]} />
+//         <h2>Tomorrow</h2>
+//         <List tasks={["Study", "Code", "Eat"]} />
+//         { /* Change code above this line */ }
+//       </div>
+//     );
+//   }
+// };
