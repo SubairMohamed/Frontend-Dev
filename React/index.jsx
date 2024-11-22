beabb8f2ca454d73c9619dc341f9c98798412c00
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-22 12:13:03 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -250,3 +250,16 @@
 //     );
 //   }
 // };
+
+// Use Default Props
+// const ShoppingCart = (props) => {
+//   return (
+//     <div>
+//       <h1>Shopping Cart Component</h1>
+//     </div>
+//   )
+// };
+// // Change code below this line
+// ShoppingCart.defaultProps = {
+//   items: 0
+// };
