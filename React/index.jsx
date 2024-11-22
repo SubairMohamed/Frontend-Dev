93c5f0193708af4841d6fedbc5ac3a2155a13995
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-22 11:57:16 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -197,3 +197,30 @@
 // }
 
 // ReactDOM.render(<MyComponent />, document.getElementById('challenge-node'));
+
+const CurrentDate = (props) => {
+  return (
+    <div>
+      { /* Change code below this line */ }
+      <p>The current date is: {props.date}</p>
+      { /* Change code above this line */ }
+    </div>
+  );
+};
+
+// Pass Props to a Stateless Functional Component
+// class Calendar extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h3>What date is it?</h3>
+//         { /* Change code below this line */ }
+//          <CurrentDate date={Date()} />
+//         { /* Change code above this line */ }
+//       </div>
+//     );
+//   }
+// };
