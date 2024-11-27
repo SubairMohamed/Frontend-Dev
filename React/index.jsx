b6bb55b3d85d0342fc49714aaf832db3afd62da3
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-27 11:53:49 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -384,3 +384,22 @@
 //     );
 //   }
 // };
+
+// Render State in the User Interface
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: 'freeCodeCamp'
+//     }
+//   }
+//   render() {
+//     return (
+//       <div>
+//         { /* Change code below this line */ }
+//    <h1>{this.state.name}</h1>
+//         { /* Change code above this line */ }
+//       </div>
+//     );
+//   }
+// };
