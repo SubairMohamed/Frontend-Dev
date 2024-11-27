bdf23ed5ee7d3847f2875139baf9d32d9eb38ebd
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-27 11:18:18 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -283,3 +283,27 @@
 //     { /* Change code above this line */ }
 //   }
 // };
+
+// Use PropTypes to Define the Props You Expect
+// const Items = (props) => {
+//   return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+// };
+
+// // Change code below this line
+// Items.propTypes = {
+//   quantity: PropTypes.number.isRequired
+// };
+// // Change code above this line
+
+// Items.defaultProps = {
+//   quantity: 0
+// };
+
+// class ShoppingCart extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return <Items />
+//   }
+// };
