6aaf0aa802cd11873854cef3feb4c0253e5a104b
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-27 11:28:25 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -341,3 +341,27 @@
 //     );
 //   }
 // };
+
+// Review Using Props with Stateless Functional Components
+// class CampSite extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Camper/>
+//       </div>
+//     );
+//   }
+// };
+// // Change code below this line
+// const Camper = props => <p>{props.name}</p>;
+
+// Camper.defaultProps = {
+//   name: "CamperBot"
+// };
+
+// Camper.propTypes = {
+//   name: PropTypes.string.isRequired
+// };
