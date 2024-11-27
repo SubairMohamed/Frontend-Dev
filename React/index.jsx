5f956a7cbcee59c64bba5f9891b8fc2054623d79
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-27 11:26:05 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -307,3 +307,37 @@
 //     return <Items />
 //   }
 // };
+
+// Access Props Using this.props
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//   }
+//   render() {
+//     return (
+//         <div>
+//             { /* Change code below this line */ }
+//             <Welcome name="Welcome"/>
+//             { /* Change code above this line */ }
+
+//         </div>
+//     );
+//   }
+// };
+
+// class Welcome extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//   }
+//   render() {
+//     return (
+//         <div>
+//           { /* Change code below this line */ }
+//           <p>Hello, <strong>{this.props.name}</strong>!</p>
+//           { /* Change code above this line */ }
+//         </div>
+//     );
+//   }
+// };
