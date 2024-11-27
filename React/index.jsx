d3a35aa2b919a5b3b4ab382fc508d7ea0d7cfd45
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-11-27 11:42:07 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -365,3 +365,22 @@
 // Camper.propTypes = {
 //   name: PropTypes.string.isRequired
 // };
+
+// Create a Stateful Component
+// class StatefulComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // Only change code below this line
+//     this.state = {
+//       firstName : "Name"
+//     }
+//     // Only change code above this line
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.firstName}</h1>
+//       </div>
+//     );
+//   }
+// };
