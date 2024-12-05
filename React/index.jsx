f25001fd9930eb40606cba8adcd6711a4a263139
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-12-05 12:24:48 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -453,3 +453,29 @@
 //     );
 //   }
 // };
+
+// Bind 'this' to a Class Method
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       text: "Hello"
+//     };
+//     // Change code below this line
+//  this.handleClick = this.handleClick.bind(this);
+//     // Change code above this line
+//   }
+//   handleClick() {
+//     this.setState({
+//       text: "You clicked!"
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick = {this.handleClick}>Click Me</button>
+//         <h1>{this.state.text}</h1>
+//       </div>
+//     );
+//   }
+// };
