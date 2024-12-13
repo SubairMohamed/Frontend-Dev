c1251956faf99dab338b41e0e3259455f84c6608
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-12-13 21:10:40 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -520,4 +520,42 @@ class MyComponent extends React.Component {
 //   }
 // };
 
-  // Write a Simple Counter
+// Write a Simple Counter
+// class Counter extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     };
+  
+// this.increment = this.increment.bind(this);
+// this.decrement = this.decrement.bind(this);
+// this.reset = this.reset.bind(this);
+//   }
+// reset() {
+//     this.setState({
+//       count: 0
+//     });
+//   }
+//   increment() {
+//     this.setState(state => ({
+//       count: state.count + 1
+//     }));
+//   }
+//   decrement() {
+//     this.setState(state => ({
+//       count: state.count - 1
+//     }));
+//   }
+//   // Change code above this line
+//   render() {
+//     return (
+//       <div>
+//         <button className='inc' onClick={this.increment}>Increment!</button>
+//         <button className='dec' onClick={this.decrement}>Decrement!</button>
+//         <button className='reset' onClick={this.reset}>Reset</button>
+//         <h1>Current Count: {this.state.count}</h1>
+//       </div>
+//     );
+//   }
+// };
