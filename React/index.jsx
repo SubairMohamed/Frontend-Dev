d02445b64bdd329565e5e7f123ea8a0896ba734c
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-12-21 20:45:40 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -738,3 +738,29 @@ class MyComponent extends React.Component {
 //     return <div />
 //   }
 // };
+
+// Use the Lifecycle Method componentDidMount
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       activeUsers: null
+//     };
+//   }
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({
+//         activeUsers: 1273
+//       });
+//     }, 2500);
+//   }
+//   render() {
+//     return (
+//       <div>
+//         {/* Change code below this line */}
+//          <h1>Active Users: { this.state.activeUsers }</h1>
+//         {/* Change code above this line */}
+//       </div>
+//     );
+//   }
+// }
