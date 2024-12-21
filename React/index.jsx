cdedf26a300b5863b7519d0db6e3d7660ffca9dc
subair mohamed <96694020+SubairMohamed@users.noreply.github.com>
2024-12-21 20:37:22 +0100

    Update index.jsx

--- React/index.jsx
+++ React/index.jsx
@@ -726,4 +726,17 @@ class MyComponent extends React.Component {
 //   }
 // };
 
-// i will starting reat as soon as posible
+// Use the Lifecycle Method componentWillMount
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   componentWillMount() {
+//     // Change code below this line
+//    console.log('Component being mounted');
+//     // Change code above this line
+//   }
+//   render() {
+//     return <div />
+//   }
+// };
